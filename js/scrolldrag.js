9cd913501b348c8172f368ee0fad338a09c3461d
luis <luis1232005@live.com>
2015-01-31 12:25:06 +0800

    first version

---
+++ js/scrolldrag.js
@@ -0,0 +1,31 @@
+function ScrollDrag(options){
+	//todo:初始化参数
+	
+	this.init();
+}
+
+ScrollDrag.prototype = {
+	init:function(){
+		//todo:初始化view
+	},
+	getScrollTop:function(){
+		//toeo:获得滚动条高度
+	},
+	scrollTo:function(top,diff){
+		//todo:滚动到
+	},
+	animateScrollTo:function(top,diff){
+		//todo:动态滚动条某个地方
+	},
+	addLazyLoad:function(elements){
+		//懒加载某些元素
+	},
+	ifInScreen:function(element){
+		//查看某个节点是否在视口内
+	},
+	initLazyLoadImgs:function(){
+		//todo:初始化view内图片的懒加载
+	}
+};
+
+//todo:继承backbone事件处理
